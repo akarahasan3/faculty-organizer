6e141e7d32ec6b377c30139bb72196c2f16b5718
akarahasan3 <akarahasan3@etf.unsa.ba>
2020-11-24 20:22:49 +0100

    validacija za dodajAktivnosti + estetika

--- js/iscrtaj.js
+++ js/iscrtaj.js
@@ -13,7 +13,6 @@ function iscrtajRaspored(div, dani, satPocetak, satKraj){
     var prviRed = document.createElement("TR");
     tabela.appendChild(prviRed);
     for(sat = satPocetak; sat<satKraj; sat++){
-        console.log("U petlji sam za sat " + sat);
         if(satnice.includes(sat)){
             var kolonaSat = document.createElement("TD");
             var kolonaSatTemp;
@@ -37,7 +36,6 @@ function iscrtajRaspored(div, dani, satPocetak, satKraj){
     }
     var i, j;
     for(i = 0; i<dani.length; i++){
-        console.log("U petlji sam za kolonu " + i);
         var red = document.createElement("TR");
         red.setAttribute("id", dani[i]);
         //red.setAttribute("id", dani[i] + i);
@@ -59,42 +57,49 @@ function iscrtajRaspored(div, dani, satPocetak, satKraj){
 
 function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj,dan){
     if(raspored == null){
-        alert("Greška - raspored nije kreiran")
+        alert("Greška - raspored nije kreiran");
+        return;
     }
     if(vrijemePocetak > 24 || vrijemePocetak<0 || vrijemeKraj<0 || vrijemeKraj>24){
-        alert("Greška - u rasporedu ne postoji dan ili vrijeme u kojem pokušavate dodati termin")
+        alert("Greška - u rasporedu ne postoji dan ili vrijeme u kojem pokušavate dodati termin");
+        return;
     }
     var a = raspored.querySelector("#tabela1");
     var b = document.getElementById(dan).rowIndex;
-    var c, i;
-    for(i = 0; i < a.rows[b].cells.length; i++){
-        
+    var c, i, j;
+    var prazan = true;
+    var postoji = false;
+    for(j = 0; j < a.rows[b].cells.length; j++){
+        if(a.rows[b].cells[j].className == "sat"+vrijemePocetak*10){
+            for(i = 0; i<(vrijemeKraj-vrijemePocetak)*2; i++){
+                if(a.rows[b].cells[j+i].innerHTML != ""){
+                    prazan = false;
+                    break;
+                }
+            }
+            postoji = true;
+            break;
+        }
     }
-    var kocka = document.createElement("TD");
+    if(postoji == false || prazan == false){
+        alert("Greška - već postoji termin u rasporedu u zadanom vremenu");
+        return;
+    }
     var sirina = parseInt((vrijemeKraj-vrijemePocetak)*2);
-    kocka.setAttribute("colspan", sirina.toString());
-    kocka.setAttribute("id", tip);
     var naslov = document.createElement("H4");
     var nastava = document.createElement("P");
     naslov.innerHTML = naziv;
     nastava.innerHTML = tip;
-    kocka.appendChild(naslov);
-    kocka.appendChild(nastava);
-    kocka.style.border = "dashed 1px black";
     for(i = 0; i < a.rows[b].cells.length; i++){
         if(a.rows[b].cells[i].className == "sat"+(vrijemePocetak*10).toString()){
             c = a.rows[b].cells[i];
         }
     }
-    console.log("c je");
-    console.log(c);
     var j;
     for(j = 0; j < a.rows[b].cells.length; j++){
         if(a.rows[b].cells[j].className == "sat"+vrijemePocetak*10){
             for(i = 1; i<(vrijemeKraj-vrijemePocetak)*2; i++){
                 var celija = a.rows[b].cells[j+1];
-                console.log(j+1);
-                console.log(celija);
                 celija.remove();
             }
             break;
@@ -104,6 +109,4 @@ function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj,dan){
     c.setAttribute("id", tip);
     c.appendChild(naslov);
     c.appendChild(nastava);
-    console.log(kocka);
-    console.log(c);
 }
